e68e9b7541942fa30fec7877573f24c57e93c00b
James Godwill Arkoh <jamesgodwillarkoh@gmail.com>
2023-04-13 18:54:54 +0000

    created header nav icon and profile image

---
+++ components/home/popular/Popularjobs.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+import styles from './popularjobs.style'
+import { View, Text } from 'react-native'
+
+const Popularjobs = () => {
+  return (
+    <View>
+      <Text>Popularjobs</Text>
+    </View>
+  )
+}
+
+export default Popularjobs
